5802681b01e0ef3a4119d22e5bb050286a05876b
cyjhunnyboy <cyjhunnyboy@sina.com>
2019-10-04 16:59:27 +0800

    通过By.className(locatorName)定位页面元素

---
+++ SeleniumDemoProj/src/test/java/per/cyj/selenium/element/FindElementByClassNameTest.java
@@ -0,0 +1,27 @@
+package per.cyj.selenium.element;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import junit.framework.Assert;
+import per.cyj.selenium.BaseTest;
+
+/**
+ * @author chenyongjun
+ * @apiNote 通过By.className(locatorName)定位页面元素
+ * @since 2019-10-04
+ */
+public class FindElementByClassNameTest extends BaseTest {
+
+	private static final String BASE_URL = "http://192.168.126.128:8080/selenium/html/class.html";
+	
+	@Test(description = "通过By.className(locatorName)定位页面元素")
+	public void testFindElementByClassName() throws Exception {
+		driver.navigate().to(BASE_URL);
+		WebElement inputBox = driver.findElement(By.className("spread"));
+		Assert.assertTrue(inputBox.isDisplayed());
+		inputBox.clear();
+		inputBox.sendKeys("光荣之路自动化测试，让你飞起来！");
+		Thread.sleep(1000);
+	}
+}
